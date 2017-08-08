34f24d309c04338880a73b827fa3bb8fa7942e6a
Kite <ixkaito@gmail.com>
2017-08-09 08:37:56 +0900

    Improve style

--- footer.php
+++ footer.php
@@ -1,17 +1,21 @@
 <?php get_sidebar(); ?>
 
-<?php if ( has_nav_menu( 'footer' ) ) : ?>
-	<?php wp_nav_menu( array(
-		'theme_location'  => 'footer',
-		'menu_id'         => 'footer-menu',
-		'container'       => 'nav',
-		'container_class' => 'footer-nav',
-	) ); ?>
-<?php endif; ?>
+<footer class="footer">
 
-<?php if ( get_theme_mod( 'copyright' ) ) : ?>
-	<p>© <?php echo esc_html( get_theme_mod( 'copyright' ) ); ?></p>
-<?php endif; ?>
+	<?php if ( has_nav_menu( 'footer' ) ) : ?>
+		<?php wp_nav_menu( array(
+			'theme_location'  => 'footer',
+			'menu_id'         => 'footer-menu',
+			'container'       => 'nav',
+			'container_class' => 'footer-nav',
+		) ); ?>
+	<?php endif; ?>
+
+	<?php if ( get_theme_mod( 'copyright' ) ) : ?>
+		<p>© <?php echo esc_html( get_theme_mod( 'copyright' ) ); ?></p>
+	<?php endif; ?>
+
+</footer>
 
 <?php wp_footer(); ?>
 
