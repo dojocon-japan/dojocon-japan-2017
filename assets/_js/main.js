b4d7d0620882173dd6924ea69843e121519055e6
Kite <ixkaito@gmail.com>
2017-08-09 22:26:14 +0900

    Improve style

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -5,7 +5,7 @@ window.$ = window.jQuery = require('jquery');
 $(function() {
 
   $('.js-hamburger').on('click touchstart', function(e) {
-    $('.js-hamburger > .hamburger').toggleClass('is-active');
+    $('.js-hamburger').toggleClass('is-active');
     e.preventDefault();
   });
 
