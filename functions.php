7d773e0da426ed2f66a3d9db82dbf98bd03a7c4e
Kite <ixkaito@gmail.com>
2017-08-28 02:37:00 +0900

    Fix a bug

--- functions.php
+++ functions.php
@@ -134,7 +134,7 @@ function dojoconjapan2017_front_page_template( $template ) {
 add_filter( 'frontpage_template', 'dojoconjapan2017_front_page_template' );
 
 function dojoconjapan2017_auto_embed_container( $content ) {
-	$content = preg_replace( '!<p>(\s*<iframe\s*src=["\'](https?:)?//www\.google\.com/maps/embed\?)!', '<p class="googlemaps-container">$1', $content );
+	$content = preg_replace( '#<p>(\s*<iframe[^>]+src=["\'](https?:)?//www\.google\.com/maps/embed\?)#', '<p class="googlemaps-container">$1', $content );
 	return $content;
 }
 add_filter( 'the_content', 'dojoconjapan2017_auto_embed_container' );
