bf41450d7f1eeee407423dd347641a5157f70c6d
Kite <ixkaito@gmail.com>
2017-08-29 04:01:51 +0900

    Improve the background image

--- templates/front-page-content/about.php
+++ templates/front-page-content/about.php
@@ -59,8 +59,8 @@
 	</div>
 
 	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="post-thumbnail">
-			<?php the_post_thumbnail(); ?>
+		<div class="background-image">
+			<?php the_post_thumbnail( 'full' ); ?>
 		</div>
 	<?php endif; ?>
 
