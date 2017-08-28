5cf920778db5bc9ccd1a18c94e8e8b27e2108a1b
Kite <ixkaito@gmail.com>
2017-08-29 02:26:44 +0900

    Add sns section

---
+++ templates/front-page-content/sns.php
@@ -0,0 +1,17 @@
+<section class="front-page-content page page-<?php echo esc_attr( $post->post_name ); ?>">
+
+	<h1 class="page-title"><?php the_title(); ?></h1>
+
+	<p class="follow-us"><span class="follow">Follow</span> <span class="us">Us!</span></p>
+
+	<?php if ( get_the_excerpt() ) : ?>
+		<div class="page-excerpt">
+			<?php the_excerpt(); ?>
+		</div>
+	<?php endif; ?>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+
+</section>
