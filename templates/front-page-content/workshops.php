5e5608be8504a2b9e8b288b5ebc1cc969a36c2f6
Kite <ixkaito@gmail.com>
2017-09-06 02:46:42 +0900

    Update workshops.php

--- templates/front-page-content/workshops.php
+++ templates/front-page-content/workshops.php
@@ -16,6 +16,40 @@
 
 		<?php the_content( '' ); ?>
 
+		<?php $_post = $post;
+		$args = array(
+			'numberposts' => -1,
+			'post_type'   => 'main-event',
+		);
+		$main_events = get_posts( $args );
+
+		if ( $main_events ) : ?>
+
+			<ul class="main-events">
+				<?php foreach ( $main_events as $post ) : setup_postdata( $post );
+					$labels = get_field( 'labels' );
+					?>
+					<li>
+						<p class="event-time">[<?php the_field( 'start-time' ); ?>−<?php the_field( 'ending-time' ); ?>]</p>
+						<h2 class="event-title"><?php the_title(); ?></h2>
+						<p class="labels">
+							<?php foreach ( $labels as $label ) : ?>
+								<span class="label-<?php echo esc_attr( $label['value'] ); ?>"><?php echo esc_html( $label['label'] ); ?></span>
+							<?php endforeach; ?>
+						</p>
+					</li>
+				<?php endforeach; wp_reset_postdata(); ?>
+			</ul>
+
+		<?php endif; ?>
+
+		<?php
+		$post = $_post;
+		setup_postdata( $post );
+		?>
+
+		<a href="<?php the_permalink(); ?>" class="button button-white">More</a>
+
 	</div>
 
 </section>
