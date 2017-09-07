a3faff0522ba07496dbc3988eef23c8cea636caa
Kite <ixkaito@gmail.com>
2017-09-08 03:47:24 +0900

    Improve the hero

--- header.php
+++ header.php
@@ -70,35 +70,37 @@
 	</span>
 </button>
 
-<div class="hero" <?php if ( get_header_image() ) : ?>
-	style="background-image: <?php header_image(); ?>;"
-<?php endif; ?>>
-	<div class="hero-text">
-		<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-		<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-		<?php if ( get_theme_mod( 'dojocon_date' ) && get_theme_mod( 'dojocon_day' ) ) : ?>
-			<p class="dojocon-day">
-				<span class="date"><?php echo esc_html( get_theme_mod( 'dojocon_date' ) ); ?></span>
-				<span class="day"><?php echo esc_html( get_theme_mod( 'dojocon_day' ) ); ?></span>
-			</p>
-		<?php endif; ?>
-		<?php if ( get_theme_mod( 'dojocon_venue' ) ) : ?>
-			<p class="dojocon-venue">
-				<span class="preposition">in</span>
-				<span class="venue"><?php echo esc_html( get_theme_mod( 'dojocon_venue' ) ); ?></span>
-			</p>
+<?php if ( is_home() || is_front_page() ) : ?>
+	<div class="hero" <?php if ( get_header_image() ) : ?>
+		style="background-image: <?php header_image(); ?>;"
+	<?php endif; ?>>
+		<div class="hero-text">
+			<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+			<p class="site-description"><a href="<?php echo esc_url( home_url( '#front-page-about' ) ); ?>"><?php bloginfo( 'description' ); ?></a></p>
+			<?php if ( get_theme_mod( 'dojocon_date' ) && get_theme_mod( 'dojocon_day' ) ) : ?>
+				<p class="dojocon-day">
+					<span class="date"><?php echo esc_html( get_theme_mod( 'dojocon_date' ) ); ?></span>
+					<span class="day"><?php echo esc_html( get_theme_mod( 'dojocon_day' ) ); ?></span>
+				</p>
+			<?php endif; ?>
+			<?php if ( get_theme_mod( 'dojocon_venue' ) ) : ?>
+				<p class="dojocon-venue">
+					<span class="preposition">in</span>
+					<span class="venue"><?php echo esc_html( get_theme_mod( 'dojocon_venue' ) ); ?></span>
+				</p>
+			<?php endif; ?>
+		</div>
+		<?php if ( ! get_header_image() ) : ?>
+			<div class="hero-image">
+				<img class="neon" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-neon.svg' ) ); ?>" alt="DojoCon Japan">
+				<div class="shadow">
+					<img class="bigtop-shadow" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-bigtop-shadow.svg' ) ); ?>">
+					<img class="people-shadow" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-people-shadow.svg' ) ); ?>">
+				</div>
+				<img class="bigtop" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-bigtop.svg' ) ); ?>">
+				<img class="people" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-people.svg' ) ); ?>">
+				<img class="andyou" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-andyou.svg' ) ); ?>">
+			</div>
 		<?php endif; ?>
 	</div>
-	<?php if ( ! get_header_image() ) : ?>
-		<div class="hero-image">
-			<img class="neon" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-neon.svg' ) ); ?>" alt="DojoCon Japan">
-			<div class="shadow">
-				<img class="bigtop-shadow" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-bigtop-shadow.svg' ) ); ?>">
-				<img class="people-shadow" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-people-shadow.svg' ) ); ?>">
-			</div>
-			<img class="bigtop" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-bigtop.svg' ) ); ?>">
-			<img class="people" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-people.svg' ) ); ?>">
-			<img class="andyou" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/hero-andyou.svg' ) ); ?>">
-		</div>
-	<?php endif; ?>
-</div>
+<?php endif; ?>
