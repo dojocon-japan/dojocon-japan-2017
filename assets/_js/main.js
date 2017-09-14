b2055955afd78a744a93a029a297dbff3a6d9293
Kite <ixkaito@gmail.com>
2017-09-15 08:06:58 +0900

    Fix bugs on global menus

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -15,23 +15,29 @@ $(function() {
    * Menu
    */
   $('.js-sliding-panel-toggle').on('click touchstart', function(e) {
-    $('.js-sliding-panel-toggle, .sliding-panel-content').toggleClass('is-active');
     e.preventDefault();
+    $('.js-sliding-panel-toggle, .sliding-panel-content').toggleClass('is-active');
   });
 
-  $('.sliding-panel-content a').on('click touchstart', function(e) {
-    $('.js-sliding-panel-toggle, .sliding-panel-content').removeClass('is-active');
-    e.preventDefault();
+  $('.sliding-panel-content a[href*="#"]').on('click touchstart', function(e) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      e.preventDefault();
+      $('.js-sliding-panel-toggle, .sliding-panel-content').removeClass('is-active');
+    }
   });
 
   $('.js-entry-nav-box-toggle').on('click touchstart', function(e) {
-    $('.entry-nav-box, .js-entry-nav-fade-screen').addClass('is-active');
     e.preventDefault();
+    $('.entry-nav-box, .js-entry-nav-fade-screen').addClass('is-active');
   });
 
   $('.js-entry-nav-box-close, .js-entry-nav-fade-screen').on('click touchstart', function(e) {
-    $('.entry-nav-box, .js-entry-nav-fade-screen').removeClass('is-active');
     e.preventDefault();
+    $('.entry-nav-box, .js-entry-nav-fade-screen').removeClass('is-active');
   });
 
   /**
