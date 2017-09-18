a08985167dd547b238f43116009da26e243f0d35
Kite <ixkaito@gmail.com>
2017-09-19 07:10:13 +0900

    Add post thumbnail to single post and circle-image

--- templates/loop-single.php
+++ templates/loop-single.php
@@ -7,6 +7,12 @@
 			<time><?php the_date(); ?></time>
 		</div>
 
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="post-thumbnail">
+				<?php the_post_thumbnail( 'full' ); ?>
+			</div>
+		<?php endif; ?>
+
 		<div class="entry-content">
 			<?php the_content(); ?>
 		</div>
