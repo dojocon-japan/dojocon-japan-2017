1152a6a24701e67e54b65c38aadf2d8483e97f27
Kite <ixkaito@gmail.com>
2017-09-27 09:04:27 +0900

    Improve the footer for desktop

--- footer.php
+++ footer.php
@@ -1,20 +1,22 @@
 <?php // get_sidebar(); ?>
 
 <footer class="footer">
+	<div class="footer-inner">
 
-	<?php if ( has_nav_menu( 'footer' ) ) : ?>
-		<?php wp_nav_menu( array(
-			'theme_location'  => 'footer',
-			'menu_id'         => 'footer-menu',
-			'container'       => 'nav',
-			'container_class' => 'footer-nav',
-		) ); ?>
-	<?php endif; ?>
+		<?php if ( has_nav_menu( 'footer' ) ) : ?>
+			<?php wp_nav_menu( array(
+				'theme_location'  => 'footer',
+				'menu_id'         => 'footer-menu',
+				'container'       => 'nav',
+				'container_class' => 'footer-nav',
+			) ); ?>
+		<?php endif; ?>
 
-	<?php if ( get_theme_mod( 'copyright' ) ) : ?>
-		<p class="copyright">© <?php echo esc_html( get_theme_mod( 'copyright' ) ); ?></p>
-	<?php endif; ?>
+		<?php if ( get_theme_mod( 'copyright' ) ) : ?>
+			<p class="copyright">© <?php echo esc_html( get_theme_mod( 'copyright' ) ); ?></p>
+		<?php endif; ?>
 
+	</div>
 </footer>
 
 <?php wp_footer(); ?>
