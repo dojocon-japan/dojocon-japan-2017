06bc9cc6901b69a0429863997f9bc0cc61e738ed
Kite <ixkaito@gmail.com>
2017-09-28 16:25:12 +0900

    Remove test

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 	<ul class="share-links">
 		<li class="fb"><?php echo do_shortcode( '[share_link sns="fb"]Facebook<br><small>で</small><br>シェア[/share_link]' ); ?></li>
 		<li class="tw"><?php echo do_shortcode( '[share_link sns="tw" hashtags="DojoConJP" via="DojoConJapan"]Twitter<br><small>で</small><br>シェア[/share_link]' ); ?></li>
-		<?php if ( !wp_is_mobile() ) : ?>
+		<?php if ( wp_is_mobile() ) : ?>
 			<li class="line"><?php echo do_shortcode( '[share_link sns="line"]LINE<br><small>で</small><br>シェア[/share_link]' ); ?></li>
 		<?php endif; ?>
 	</ul>
