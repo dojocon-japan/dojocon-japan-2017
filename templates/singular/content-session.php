6556c9207cee2b1bf9415af33e74ece9140bd3db
Kite <ixkaito@gmail.com>
2017-10-08 15:47:52 +0900

    Add single session content

---
+++ templates/singular/content-session.php
@@ -0,0 +1,51 @@
+<main class="main">
+
+	<?php while( have_posts() ): the_post(); ?>
+
+		<div class="post-meta">
+			<h1 class="post-title"><?php the_title(); ?></h1>
+			<?php $categories = get_the_terms( $post->ID, 'session-category' ); ?>
+			<p>
+				<?php foreach ( $categories as $cat ) : ?>
+					<span class="session-category"><?php echo esc_html( $cat->name ); ?></span>
+				<?php endforeach; ?>
+			</p>
+			<time>[<?php the_field( 'start-time' ); ?> - <?php the_field( 'ending-time' ); ?>]</time>
+		</div>
+
+		<?php if ( ! get_field( 'hide-post-thumbnail' ) && has_post_thumbnail() ) : ?>
+			<div class="post-thumbnail">
+				<?php the_post_thumbnail( 'full' ); ?>
+			</div>
+		<?php endif; ?>
+
+		<div class="entry-content">
+
+			<?php the_content(); ?>
+
+			<?php $speakers = new WP_Query( array(
+				'post_type' => 'speaker',
+				'post__in' => get_field( 'speakers' ),
+			) );
+
+			if ( $speakers->have_posts() ) : ?>
+
+				<h2><?php _e( 'Speaker', 'dojocon-japan-2017' ); ?></h2>
+
+				<div class="speakers">
+
+					<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
+
+						<h3 class="speaker-name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+					<?php endwhile; ?>
+
+				</div>
+
+			<?php endif; wp_reset_postdata(); ?>
+
+		</div>
+
+	<?php endwhile ?>
+
+</main>
