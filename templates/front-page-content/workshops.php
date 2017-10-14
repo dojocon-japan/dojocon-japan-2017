3d60b2ca517dd69321e5cd2dc0919af3af1d4d46
Kite <ixkaito@gmail.com>
2017-10-15 08:21:22 +0900

    Improve timetable button in workshop section

--- templates/front-page-content/workshops.php
+++ templates/front-page-content/workshops.php
@@ -104,8 +104,12 @@
 		setup_postdata( $post );
 
 		$timetable = get_page_by_path( 'timetable' );
-		if ( $timetable ) : ?>
-			<a href="<?php echo esc_url( get_the_permalink( $timetable ) ); ?>" class="button button-white timetable-button">Timetable</a>
+
+		if ( 'publish' == get_post_status( $timetable ) ) : ?>
+			<div class="timetable-button-outer">
+				<a href="<?php echo esc_url( get_the_permalink( $timetable ) ); ?>" class="button button-white button-block timetable-button">Timetable</a>
+				<p>セッション・ワークショップのタイムテーブル</p>
+			</div>
 		<?php endif; ?>
 
 	</div>
